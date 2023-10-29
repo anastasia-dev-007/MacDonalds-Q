47ea29fcc103aac05fe3ee1980fb7f79507520cf
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-10-29 20:55:25 +0200

    Changed code

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [order, setOrder] = useState([]);
-  const [pendingOrder, setPendingOrder] = useState([]);
-  const [preparingOrder, setPreparingOrder] = useState([]);
-  const [doneOrder, setDoneOrder] = useState([]);
-
   const menu = [
     {
       id: 1,
@@ -26,45 +21,43 @@ function App() {
     }
   ];
 
-  const addToOrder = (item) => {
+  const [orders, setOrders] = useState([]);
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const handleItemClick = (item) => {
+      setSelectedItems([...selectedItems, item]);
+  };
+
+  const createOrder = () => {
+    const time = Date.now(); // Get the current timestamp
+
     const newOrder = {
-      ...item,
-      status: 'pending',
+      id: orders.length + 1,
+      menuItems: selectedItems,
+      status: 'pending', // Set the initial status to 'pending'
+      time: time,
     };
-    setOrder([...order, newOrder]);
+
+    console.log(newOrder);
+
+    // Add the new order to the orders array
+    setOrders([...orders, newOrder]);
+
+    // Clear the selected items for the next order
+    setSelectedItems([]);
   };
 
-  const finishOrder = () => {
-    if (order.length > 0) {
-      const updatedStatus = order.map(item => ({
-        ...item,
-        status: 'preparing',
-      }));
-      setPendingOrder([...pendingOrder, ...updatedStatus]);
-      setOrder([]);
-    }
-  };
+  const markOrderAsPreparing = (orderId) => {
+    // Find the order by its ID and update its status to 'preparing'
+    const updatedOrders = orders.map((order) => {
+      if (order.id === orderId) {
+        return { ...order, status: 'preparing', timePreparing: Date.now() };
+      }
+      return order;
+    });
 
-  const moveToPreparing = (order) => {
-    if (pendingOrder.length > 0) {
-      const updatedStatus = order.map(item => ({
-        ...item,
-        status: 'done',
-      }));
-      setPreparingOrder([...preparingOrder, ...updatedStatus]);
-      setPendingOrder(pendingOrder.filter((pending) => pending !== order));
-    }
-  };
-
-  const moveToDone = (order) => {
-    if (preparingOrder.length > 0) {
-      const updatedStatus = order.map(item => ({
-        ...item,
-        status: 'paid',
-      }));
-      setDoneOrder([...doneOrder, ...updatedStatus]);
-      setPreparingOrder(preparingOrder.filter((done) => done !== order));
-    }
+    // Update the orders in the state
+    setOrders(updatedOrders);
   };
 
   return (
@@ -75,11 +68,9 @@ function App() {
         <div className="clientInterface">
           <div className="menu">
             <h4>Menu</h4>
-
             <ul>
               {menu.map((item) => (
-                <li key={item.id}
-                  onClick={() => addToOrder(item)}>
+                <li key={item.id} onClick={() => handleItemClick(item)}>
                   {item.title}
                 </li>
               ))}
@@ -89,20 +80,16 @@ function App() {
           <div className="order">
             <h4>Order</h4>
             <ul>
-              {order.length === 0 ? (
-                <p>Please select items from the menu to create your order!</p>
-              ) : (
-                order.map(item => (
-                  <li key={item.id}>
-                    {item.title}
-                  </li>
-                )))}
+              {selectedItems.map((item) => (
+                <li key={item.id}>{item.title}</li>
+              ))}
             </ul>
           </div>
         </div>
 
-        <button disabled={order.length === 0} onClick={() => finishOrder()}>Finish Order</button>
+        <button onClick={createOrder}>Finish Order</button>
       </div>
+
 
       <hr />
 
@@ -112,66 +99,45 @@ function App() {
         <div className="kitchenInterface">
           <div className="pending">
             <h4>Pending</h4>
-
             <ol>
-              {pendingOrder.length === 0 ? (
-                <p>No pending orders yet!</p>
-              ) : (
-                pendingOrder
-                  .filter(order => order.some(item => item.status === 'pending'))
-                  .map((order, index) => (
-                    <li key={index} onClick={() => moveToPreparing(order)}>
-                      <h5>Order {index + 1}</h5>
+              {orders
+                .filter((order) => order.status === 'pending')
+                .map((order) => (
+                  <li key={order.id} onClick={() => markOrderAsPreparing(order.id)}>
+                    <p>Order {order.id}</p>
+                    {order.menuItems.map((item) => (
                       <ul>
-                        {order.map(item => (
-                          <li key={item.id}>{item.title}</li>
-                        ))}
+                        <li key={item.id}>{item.title}</li>
                       </ul>
-                    </li>
-                  ))
-              )}
+                    ))}
+                  </li>
+                ))}
             </ol>
-
           </div>
 
           <div className="preparing">
             <h4>Preparing</h4>
             <ol>
-            {preparingOrder.length === 0 ? (
-                <p>No preparing orders yet!</p>
-              ) : (
-              preparingOrder
-                .filter(order => order.some(item => item.status === 'preparing'))
-                .map((order, index) => (
-                  <li key={index}
-                    onClick={() => moveToDone(order)}>
-                    <h5>Order {index + 1}</h5>
-                    <ul>
-                      {order.map(item =>
-                        <li key={item.id}>{item.title}</li>)}
-                    </ul>
-                  </li>)))}
+              {orders
+                .filter((order) => order.status === 'preparing')
+                .sort((a, b) => a.timePreparing - b.timePreparing) // Sort by the time they were marked as preparing
+                .map((order) => (
+                  <li key={order.id}>
+                    <p>Order {order.id}</p>
+                    {order.menuItems.map((item) => (
+                      <ul>
+                        <li key={item.id}>{item.title}</li>
+                      </ul>
+                    ))}
+                  </li>
+                ))}
             </ol>
-
           </div>
 
           <div className="done">
             <h4>Done</h4>
             <ol>
-            {doneOrder.length === 0 ? (
-                <p>No done orders yet!</p>
-              ) : (
-              doneOrder
-                .filter(order => order.some(item => item.status === 'done'))
-                .map((order, index) => (
-                  <li key={index}>
-                    <h5>Order {index + 1}</h5>
-                    <ul>
-                      {order.map(item =>
-                        <li key={item.id}>{item.title}</li>)}
-                    </ul>
-                  </li>
-                )))}
+              {/* Add done orders here */}
             </ol>
           </div>
         </div>
