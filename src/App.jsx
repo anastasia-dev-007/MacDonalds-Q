5dfc508c80de32fa69e8d8c86c5b6da28a3cd033
anastasia-dev-007 <133599958+anastasia-dev-007@users.noreply.github.com>
2023-10-29 22:05:05 +0200

    added functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,15 @@ function App() {
     {
       id: 4,
       title: 'Ice-Cream'
-    }
+    },
+    {
+      id: 5,
+      title: 'Happy-Meal'
+    },
+    {
+      id: 6,
+      title: 'Milkshake'
+    },
   ];
 
   const [orders, setOrders] = useState([]);
@@ -29,26 +37,21 @@ function App() {
   };
 
   const createOrder = () => {
-    const time = Date.now(); // Get the current timestamp
+    const time = Date.now(); 
 
     const newOrder = {
       id: orders.length + 1,
       menuItems: selectedItems,
-      status: 'pending', // Set the initial status to 'pending'
+      status: 'pending', 
       time: time,
     };
 
-    console.log(newOrder);
-
-    // Add the new order to the orders array
     setOrders([...orders, newOrder]);
 
-    // Clear the selected items for the next order
     setSelectedItems([]);
   };
 
-  const markOrderAsPreparing = (orderId) => {
-    // Find the order by its ID and update its status to 'preparing'
+  const moveToPreparing = (orderId) => {
     const updatedOrders = orders.map((order) => {
       if (order.id === orderId) {
         return { ...order, status: 'preparing', timePreparing: Date.now() };
@@ -56,7 +59,22 @@ function App() {
       return order;
     });
 
-    // Update the orders in the state
+    setOrders(updatedOrders);
+  };
+
+  const moveToDone = (orderId) => {
+    const updatedOrders = orders.map((order) => {
+      if (order.id === orderId) {
+        return { ...order, status: 'done', timeDone: Date.now() };
+      }
+      return order;
+    });
+
+    setOrders(updatedOrders);
+  };
+
+  const pickUpOrder = (orderId) => {
+    const updatedOrders = orders.filter((order) => order.id !== orderId);
     setOrders(updatedOrders);
   };
 
@@ -87,7 +105,7 @@ function App() {
           </div>
         </div>
 
-        <button onClick={createOrder}>Finish Order</button>
+        <button disabled = {selectedItems.length === 0} onClick={createOrder}>Finish Order</button>
       </div>
 
 
@@ -103,7 +121,7 @@ function App() {
               {orders
                 .filter((order) => order.status === 'pending')
                 .map((order) => (
-                  <li key={order.id} onClick={() => markOrderAsPreparing(order.id)}>
+                  <li key={order.id} onClick={() => moveToPreparing(order.id)}>
                     <p>Order {order.id}</p>
                     {order.menuItems.map((item) => (
                       <ul>
@@ -122,7 +140,8 @@ function App() {
                 .filter((order) => order.status === 'preparing')
                 .sort((a, b) => a.timePreparing - b.timePreparing) // Sort by the time they were marked as preparing
                 .map((order) => (
-                  <li key={order.id}>
+                  <li key={order.id}
+                  onClick={() => moveToDone(order.id)}>
                     <p>Order {order.id}</p>
                     {order.menuItems.map((item) => (
                       <ul>
@@ -137,7 +156,20 @@ function App() {
           <div className="done">
             <h4>Done</h4>
             <ol>
-              {/* Add done orders here */}
+            {orders
+                .filter((order) => order.status === 'done')
+                .sort((a, b) => a.timeDone - b.timeDone) // Sort by the time they were marked as preparing
+                .map((order) => (
+                  <li key={order.id}>
+                    <p>Order {order.id}</p>
+                    {order.menuItems.map((item) => (
+                      <ul>
+                        <li key={item.id}>{item.title}</li>
+                      </ul>
+                    ))}
+                    <button onClick={() => pickUpOrder(order.id)}>Pick-up</button>
+                  </li>
+                ))}
             </ol>
           </div>
         </div>
